79e565bd4157767e378505821e20adc93ee34721
Molly Hanes <mollyfhanes@gmail.com>
2018-03-28 11:19:45 -0500

    ready to begin 'detecting end of game'

--- game/index.js
+++ game/index.js
@@ -1,22 +1,23 @@
-import {Map} from 'immutable'
-
-let board = Map()
+import { Map } from 'immutable'
 
 export const move = (turn, position) => {
-  return {
-    type: 'move',
-    position: position,
-    turn: turn
-  };
+	return {
+		type: 'move',
+		turn: turn,
+		position: position
+	}
 }
+const initialState = { board: Map(), turn: 'X' }
 
-export default function reducer(state = { board: board, turn: "X"}, action) {
-  //let newTurn = action.turn === "X" ? "O" : "X";
-  switch (action.type) {
-    case "move":
-      return { board: board.setIn(action.position), turn: "X" }
-    default:
-      return state;
-  }
+export default function reducer(state = initialState, action) {
+	let newTurn = action.turn === 'X' ? 'O' : 'X'
+	switch (action.type) {
+		case 'move':
+			return {
+				board: state.board.setIn(action.position, state.turn),
+				turn: newTurn
+			}
+		default:
+			return state
+	}
 }
-
